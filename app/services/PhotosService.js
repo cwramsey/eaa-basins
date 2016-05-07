f89387ab107170dce541a3d5ad3bd97e302d96ec
chrisramsey <cramsey@helpsocial.com>
2016-05-07 16:26:22 -0500

    commit all

--- app/services/PhotosService.js
+++
@@ -1,9 +0,0 @@
-angular.module('app').service('PhotosService', ($http, CONSTANTS) => {
-    return {
-        all() {
-            return $http.get(`${CONSTANTS.API_URL}/photos`)
-                .then(res => res.data)
-                .catch(err => console.error(err));
-        }
-    }
-});
